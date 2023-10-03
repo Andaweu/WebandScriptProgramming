333a848e5ff4ac804e7e720d58efdcc3fe079b28
Andaweu <andrew.chester@ontariotechu.net>
2023-10-03 00:35:16 -0400

    Update 2

--- Assignment 1/Assignment1Javascript.js
+++ Assignment 1/Assignment1Javascript.js
@@ -2,3 +2,21 @@ function playSound(){
     var sounds = new Audio('goofysounds.mp3')
     sounds.play()
 }
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
